bf0ac45a6352ab4228528bd49474df29d21c4f6b
Mert Erciyas <99058235@mydavinci.nl>
2022-02-22 14:27:41 +0100

    Yatzee WIP 2

--- Opdrachten/Yatzee.py
+++ Opdrachten/Yatzee.py
@@ -14,13 +14,43 @@ def fancyDice(diceNum=5):
     5:{1:' ------- ',2:' ------- ',3:' ------- ',4:' ------- ',5:' ------- ',6:' ------- '}
     }
 
-    for i in range(5): #5 is for the 5 lines that de dice art requires
+    for i in range(5): #5 is for the 5 lines that de dice art requiresx
         for args in diceRoll: #loops through the list entries
             stdout.write(diceDict[i+1][args])
             stdout.write('   ')
         stdout.write('\n')
- 
-def scoreBoard():
-    scoreBoard = {'one: 0','two: 0','three: 0','four: 0','four: 0','five: 0','six: 0'}
 
-fancyDice()
+def rollDice(num):
+    diceList = []
+    for i in range(num):
+        diceList.append(randint(1,6))
+    return diceList
+
+c = []
+def workingDice():
+    global c
+    for i in range(3):
+        e =  5 - len(c)
+        fancyDice(e)
+        b = input("Which dice do you choose?\n")
+        z = b.replace(' ','').split(',')
+        for x in z:
+            c.append(x)
+        print(c)
+        for x in c:
+            if not isinstance(x,int):
+                if x.isdigit():
+                    d = c.index(x)
+                    c[d] = int(x)
+    print(c)  
+
+
+def main():
+    workingDice()
+
+main()
+
+
+
+#def scoreBoard():
+#    scoreBoard = {'one: 0','two: 0','three: 0','four: 0','four: 0','five: 0','six: 0'}
